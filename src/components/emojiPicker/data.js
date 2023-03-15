ee1bd01f5d9ee9d48044ceed5a2abed71c206b73
Tomascba <tomialonsoo.96@gmail.com>
2023-03-15 15:48:43 -0300

    second commit

--- src/components/emojiPicker/data.js
+++ src/components/emojiPicker/data.js
@@ -19,8 +19,38 @@ export const data = [{
     keywords: "lion"
 },
 {
-    symbol: "🎈",
-    name: "red baloon",
-    keywords: "baloon party"
+    symbol: "😴",
+    name: "sleeping face",
+    keywords: "sleep face tired"
+},
+{
+    symbol: "😆",
+    name: "laugh face",
+    keywords: "face laugh lol"
+},
+{
+    symbol: "🤑",
+    name: "money face",
+    keywords: "money face rich"
+},
+{
+    symbol: "😡",
+    name: "angry face",
+    keywords: "angry mad face"
+},
+{
+    symbol: "🥶",
+    name: "cold face",
+    keywords: "cold face freeze"
+},
+{
+    symbol: "😍",
+    name: "inlove face",
+    keywords: "love face heart eyes"
+},
+{
+    symbol: "👽",
+    name: "alien face",
+    keywords: "alien ovni ufo"
 },
 ]
